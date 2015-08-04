ae903c243c2a6ab5a315690d223774e5f332b67e
Artem Timoshenko <artem_timoshenko@epam.com>
2015-08-04 14:12:50 +0300

    service getJSON

---
+++ readme.md
@@ -0,0 +1,3 @@
+1. execute npm install
+2. execute node service.js
+3. open http://localhost:3000/ in your browser
--- service.js
+++ service.js
@@ -1,1 +1,20 @@
+var express = require('express');
+var http = require('http');
+var app = express();
 
+
+app.set('port', 3000);
+
+app.get('/', function(req, res) {
+    res.send('mrc-bim Service');
+});
+
+app.get('/defaultValues', function(req, res) {
+
+    var defaultValues = require('./defaultValues.json');
+    res.send(defaultValues);
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
